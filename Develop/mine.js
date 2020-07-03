c65b097e3f618e2021dd5c2be1729756d3d5d756
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-07-02 21:07:59 -0400

    if to check

--- Develop/mine.js
+++ Develop/mine.js
@@ -74,11 +74,37 @@ const randomNumber =
 const randomSpecChar =
   specialCharArray[Math.floor(Math.random() * numberArray.length)];
 
+//prompt on screen to decide what to include in the password
+const critLower = confirm("Would you like lowercase letters?");
+const critUpper = confirm("Would you like to use uppercase letters?");
+const critSpecial = confirm("Would you like special characters?");
+const critNumber = prompt(
+  "How many characters would you like? Enter a number between 8 and 128."
+);
+
+//log to console if working
+if (critLower === true) {
+  console.log(randomLowerLetter);
+}
+
+if (critUpper === true) {
+  console.log(randomUpperLetter);
+}
+
+if (critSpecial === true) {
+  console.log(randomSpecChar);
+}
+
+if (critNumber === 8 < 128) {
+  console.log(critNumber);
+}
+
 //provided function code, changed var to const:
 function writePassword() {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
 
+  // added
   const length = prompt("Choose a number between 8 and 128.");
   if (length === 8) {
     alert("good");
