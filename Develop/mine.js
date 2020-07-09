002b2889a7ffff905d153f97c0adbf1ddf42f247
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-07-09 18:21:27 -0400

    updated html & js

--- Develop/mine.js
+++ Develop/mine.js
@@ -1,156 +1,112 @@
 //provided var, this traverses the DOM to grab the generate ID, changed to const
 const generateBtn = document.querySelector("#generate");
 
+// hook onto the id's of the checked boxes
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numberCheck = document.querySelector("#number");
+const symbolCheck = document.querySelector("#symbol");
+const passwordLength = document.querySelector("#passwordLength");
+
 // arrays that hold our information, there are probably a million better/cleaner ways to do this, but this is how we will do it for now
+const lowercaseArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const uppercaseArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const specialCharArr = [
+  "`",
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+  "=",
+  "-",
+  "{",
+  "}",
+  "|",
+  "[",
+  "]",
+  "\\",
+  ":",
+  '"',
+  ";",
+  "'",
+  "<",
+  ">",
+  "?",
+  ",",
+  ".",
+  "/",
+];
+const numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// allArr to be appended depending on what box is checked
+const allArr = [];
 
-let allArray = {
-  letterArray: [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "x",
-    "y",
-    "z",
-  ],
-  upperArray: [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-  ],
-  specialCharArray: [
-    "`",
-    "~",
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "_",
-    "+",
-    "=",
-    "-",
-    "{",
-    "}",
-    "|",
-    "[",
-    "]",
-    "\\",
-    ":",
-    '"',
-    ";",
-    "'",
-    "<",
-    ">",
-    "?",
-    ",",
-    ".",
-    "/",
-  ],
-  numberArray: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-};
+// provided function code, changed var to const:
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
-//randomize the output of each array, again, probably better ways to do this, but this is where we're starting
-const randomObject = {
-  randomLowerLetter:
-    allArray.letterArray[
-      Math.floor(Math.random() * allArray.letterArray.length)
-    ],
-  randomUpperLetter:
-    allArray.upperArray[Math.floor(Math.random() * allArray.upperArray.length)],
-  randomNumber:
-    allArray.numberArray[
-      Math.floor(Math.random() * allArray.numberArray.length)
-    ],
-  randomSpecChar:
-    allArray.specialCharArray[
-      Math.floor(Math.random() * allArray.numberArray.length)
-    ],
-};
-//get a random letter from the array
-let critLower = confirm("Would you like lowercase letters?");
-{
-  if (true) {
-    let critLower = randomObject.randomLowerLetter;
-    // console.log is working here because it is inside the scope of this definition of critLower, it will not work outside of it because at that point the def of critLower is only true, this needs to be fixed
-    console.log(critLower);
-  }
+  passwordText.value = password;
 }
-
-// const critUpper = confirm("Would you like to use uppercase letters?");
-// const critSpecial = confirm("Would you like special characters?");
-// const critNumber = prompt(
-//   "How many characters would you like? Enter a number between 8 and 128."
-// );
-
-// console.log(randomObject.randomLowerLetter + randomObject.randomNumber);
-//prompt on screen to decide what to include in the password
-
-//log to console if working
-// if (critLower === true) {
-//   console.log(randomLowerLetter);
-// }
-
-// if (critUpper === true) {
-//   console.log(randomUpperLetter);
-// }
-
-// if (critSpecial === true) {
-//   console.log(randomSpecChar);
-// }
-
-// if (critNumber === 8 < 128) {
-//   console.log(critNumber);
-// }
-
-//provided function code, changed var to const:
-// function writePassword() {
-//   const password = generatePassword();
-//   const passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// generateBtn.addEventListener("click", writePassword);
