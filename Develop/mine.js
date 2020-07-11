9db0de0a9d0240f847e90c98053bdfbb51794f4b
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-07-10 21:24:00 -0400

    added click events

--- Develop/mine.js
+++ Develop/mine.js
@@ -1,13 +1,11 @@
 //provided var, this traverses the DOM to grab the generate ID, changed to const
 const generateBtn = document.querySelector("#generate");
-
 // hook onto the id's of the checked boxes
 const uppercaseCheck = document.querySelector("#uppercase");
 const lowercaseCheck = document.querySelector("#lowercase");
 const numberCheck = document.querySelector("#number");
 const symbolCheck = document.querySelector("#symbol");
 const passwordLength = document.querySelector("#passwordLength");
-
 // arrays that hold our information, there are probably a million better/cleaner ways to do this, but this is how we will do it for now
 const lowercaseArr = [
   "a",
@@ -65,7 +63,7 @@ const uppercaseArr = [
   "Y",
   "Z",
 ];
-const specialCharArr = [
+const symbolArr = [
   "`",
   "~",
   "!",
@@ -100,8 +98,6 @@ const specialCharArr = [
   "/",
 ];
 const numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// allArr to be appended depending on what box is checked
-const allArr = [];
 
 // provided function code, changed var to const:
 function writePassword() {
@@ -109,4 +105,23 @@ function writePassword() {
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+// capture the mouse click on the checkbox
+uppercaseCheck.addEventListener("click", clickEvent);
+lowercaseCheck.addEventListener("click", clickEvent);
+symbolCheck.addEventListener("click", clickEvent);
+numberCheck.addEventListener("click", clickEvent);
+generateBtn.addEventListener("click", clickEvent);
+
+// lower level functions ******************************************
+
+// function for click event
+function clickEvent(event) {
+  console.log("added");
+}
+
+// function that takes in an array and spits out a random character
+function randomCharacter(arr) {
+  console.log(arr);
 }
